8237a757b4ef63c7829493ef1fd7eb8ad653a042
Rhenan Folco <rhenanfolco@hotmail.com>
2021-11-23 23:57:24 +1100

    Fix typo

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const pgp = require('pg-promise')();
 //connection string
 //  const con = 'postgres://postgres:rajgopal@localhost:5432/project5'
 
-const con = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATEBASE}`;
+const con = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 //  const db= pgp({ con:isProduction ? process.env.DB_DATEBASE_URL:con })
 
 //create new instance of database
