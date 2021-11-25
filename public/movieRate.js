c044f253d5545babf772c3bb04179c738b6f1a18
Rhenan Folco <rhenanfolco@hotmail.com>
2021-11-25 20:17:13 +1100

    Fix movies not displaying after user login bug

--- public/movieRate.js
+++ public/movieRate.js
@@ -1,21 +1,20 @@
-console.log("linked to ratings ..")
+console.log('linked to ratings ..');
 
-const img_base_url = "https://image.tmdb.org/t/p/w154"
-const img_base_url1 = "https://image.tmdb.org/t/p/w780/"
-  $.getJSON("https://api.themoviedb.org/3/movie/top_rated?api_key=7063359f4e85964f78dd5c2aa4165728")
+const img_base_url = 'https://image.tmdb.org/t/p/w154';
+const img_base_url1 = 'https://image.tmdb.org/t/p/w780/';
+$.getJSON(
+  'https://api.themoviedb.org/3/movie/top_rated?api_key=7063359f4e85964f78dd5c2aa4165728'
+)
 
   //$.getJSON("https://api.themoviedb.org/3/movie/now_playing?api_key=7063359f4e85964f78dd5c2aa4165728")
-  
-  .then((data)=>{
-  
-    const {results}= data
-    console.log(results)
-    let output ='';
 
-    results.forEach(movie=> {
+  .then((data) => {
+    const { results } = data;
+    console.log(results);
+    let output = '';
 
-
-      output+=`  
+    results.forEach((movie) => {
+      output += `  
       <div class ="col-md-3">
        <div class ="well text-center">
         <form  method ="post" action="ratings/${movie.id}" >
@@ -28,37 +27,30 @@ const img_base_url1 = "https://image.tmdb.org/t/p/w780/"
          </div>
          </div>
          </div>
-              `
-
-            })   
-      $("#topratedmovies").html(output)
- 
-
-  })
-  
-  .catch((error)=>{
-    console.log(error)
+              `;
+    });
+    $('#movies').html(output);
   })
 
-  
- 
-
+  .catch((error) => {
+    console.log(error);
+  });
 
 function movieSelected(id) {
   sessionStorage.setItem('movieId', id);
-   console.log(sessionStorage.getItem('movieId'))
+  console.log(sessionStorage.getItem('movieId'));
   $.post(`/ratings/${sessionStorage.getItem('movieId')}`, {
-    data:`${sessionStorage.getItem('movieId')}`,
-    success:true
+    data: `${sessionStorage.getItem('movieId')}`,
+    success: true,
   })
-  .then((response)=>{
-    console.log(response)
-  })
-  .catch((error)=>{
-    console.log(error)
-  })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   window.location = 'ratings';
-  
-console.log(response.data)
+
+  console.log(response.data);
   return false;
 }
