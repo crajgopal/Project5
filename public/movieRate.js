da6dc1d2cb190a866213d46f5bad5be8ffbd9119
Vlad Vojvo <vlad.vojvo86@gmail.com>
2021-11-25 17:33:34 +1100

    upload for Rajh

--- public/movieRate.js
+++ public/movieRate.js
@@ -1,147 +1,64 @@
-console.log('linked to ratings ..');
+console.log("linked to ratings ..")
 
-const img_base_url = 'https://image.tmdb.org/t/p/w154';
-const img_base_url1 = 'https://image.tmdb.org/t/p/w780/';
+const img_base_url = "https://image.tmdb.org/t/p/w154"
+const img_base_url1 = "https://image.tmdb.org/t/p/w780/"
+  $.getJSON("https://api.themoviedb.org/3/movie/top_rated?api_key=7063359f4e85964f78dd5c2aa4165728")
 
-// Function that renders movies in the homepage
+  //$.getJSON("https://api.themoviedb.org/3/movie/now_playing?api_key=7063359f4e85964f78dd5c2aa4165728")
+  
+  .then((data)=>{
+  
+    const {results}= data
+    console.log(results)
+    let output ='';
 
-const displayMovies = (data) => {
-  const { results } = data;
-  let output = '';
-  results.forEach((movie) => {
-    output += `  
-   <div class ="col-md-3">
-    <div class ="well text-center">
-     <img src ="${img_base_url}${movie.poster_path}">
-      <h5>${movie.title}</h5>
-      <h4>Rating :${movie.vote_average}</h4>
-      <form  method ="post" action="/movie" onclick ="movieSelected(${movie.id})" class ="btn btn-primary" id ="movieButton" type ="submit" >Movie Details</form>
-      </div>
-      </div>
-       `;
-  });
-  $('#movies').html(output);
-};
+    results.forEach(movie=> {
 
-// Function that fetches movies filtered by selected genre from The MovieDB
 
-const filterByGenre = () => {
-  $('#search-field').val('');
-  const selectedGenres = [];
-  $('#genre-checkboxes-container input[type=checkbox]:checked').each((i, v) => {
-    selectedGenres.push($(v).attr('id'));
-  });
-  const genreQueryString = selectedGenres.join('|');
-
-  $.getJSON(
-    `https://api.themoviedb.org/3/discover/movie?api_key=7063359f4e85964f78dd5c2aa4165728&with_genres=${genreQueryString}`
-  )
-    .then((data) => {
-      displayMovies(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-function getMovies(searchText) {
-  $.getJSON(
-    `https://api.themoviedb.org/3/search/movie?api_key=7063359f4e85964f78dd5c2aa4165728&query=${searchText}`
-  )
-    .then((data) => {
-      const { results } = data;
-      let output = '';
-      results.forEach((movie) => {
-        output += `  
-     <div class ="col-md-3">
-      <div class ="well text-center">
-       <img src ="${img_base_url}${movie.poster_path}">
-        <h5>${movie.title}</h5>
-        <h4>Rating :${movie.vote_average}</h4>
-        <form  method ="post" action="/movie" onclick ="movieSelected(${movie.id}) ;getMovie()" class ="btn btn-primary" id ="movieButton" type ="submit" >Movie Details</form>
-        </div>
-        </div>
-         `;
-      });
-      $('#movies').empty();
-
-      $('#movies').html(output);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-$.getJSON(
-  'https://api.themoviedb.org/3/discover/movie?api_key=7063359f4e85964f78dd5c2aa4165728'
-)
-
-  .then((data) => {
-    const { results } = data;
-    console.log(results);
-    let output = '';
-
-    results.forEach((movie) => {
-      output += `  
+      output+=`  
       <div class ="col-md-3">
        <div class ="well text-center">
+        <form  method ="post" action="ratings/${movie.id}" >
         <img src ="${img_base_url}${movie.poster_path}">
          <h5>${movie.title}</h5>
          <h4>Release date :${movie.release_date}</h4>
          <h4>Rating :${movie.vote_average}</h4>
-         <form  method ="post" action="ratings/movie" onclick ="movieSelected(${movie.id})" class ="btn btn-primary" id ="movieButton" type ="submit" >Rate this movie</form>
+         <button onclick ="movieSelected(${movie.id})" class ="btn btn-primary" id ="movieButton" type ="submit" >Rate this movie </button></form>
 
          </div>
          </div>
          </div>
-              `;
-    });
-    $('#movies').html(output);
+              `
+
+            })   
+      $("#topratedmovies").html(output)
+ 
+
+  })
+  
+  .catch((error)=>{
+    console.log(error)
   })
 
-  .catch((error) => {
-    console.log(error);
-  });
+  
+ 
 
-$(document).ready(() => {
-  $('#searchForm').on('submit', (e) => {
-    e.preventDefault();
-    let searchText = $('#search-field').val();
-    $('#genre-checkboxes-container input[type=checkbox]:checked').prop(
-      'checked',
-      false
-    );
-    getMovies(searchText);
-  });
-});
-
-// Fetches existing movie genres from The MovieDB and append them as checkboxes to the navbar genre filter list
-
-$.getJSON(
-  'https://api.themoviedb.org/3/genre/movie/list?api_key=7063359f4e85964f78dd5c2aa4165728'
-)
-  .then((genresData) => {
-    genresData.genres.forEach((genreObj) => {
-      $('#genre-checkboxes-container').append(
-        `
-      <li>
-        <div class="form-check">
-          <input id="${genreObj.id}" class="form-check-input" type="checkbox" value="" onchange="filterByGenre()">
-          <label class="form-check-label" for="${genreObj.id}">
-            ${genreObj.name}
-          </label>
-        </div>
-      </li>
-      `
-      );
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
 function movieSelected(id) {
   sessionStorage.setItem('movieId', id);
-  window.location = 'ratings/movieratings';
+   console.log(sessionStorage.getItem('movieId'))
+  $.post(`/ratings/${sessionStorage.getItem('movieId')}`, {
+    data:`${sessionStorage.getItem('movieId')}`,
+    success:true
+  })
+  .then((response)=>{
+    console.log(response)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+  window.location = 'ratings';
+  
+console.log(response.data)
   return false;
 }
